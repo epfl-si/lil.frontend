9e21dd42c9e8571c66a1dd0a3848e7c1f9eca92f
Jch4ipas <jeremy.wuersch@gmail.com>
2026-05-07 11:40:53 +0200

    [feature] translate everything in StorageTable and in BarcodeDetailPage

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,14 +1,15 @@
 import {StorageTable} from "@/components/pages/StorageTable.tsx";
 import type {State} from "@epfl-si/react-appauth";
+import { useTranslation } from 'react-i18next';
 
 export const Body = ({ oidc }: { oidc: State }) => {
-
+  const { t, i18n } = useTranslation();
   return (
     <div className="p-4 sm:p-8 max-w-6xl mx-auto w-full">
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 gap-4">
         <div>
-          <h1 className="text-2xl sm:text-3xl font-bold">Gestion des lieux de Stockage</h1>
-          <p className="text-gray-500 text-sm mt-1">Gérez vos emplacements.</p>
+          <h1 className="text-2xl sm:text-3xl font-bold">{t('app.storageFacilityManagement')}</h1>
+          <p className="text-gray-500 text-sm mt-1">{t('app.manageYourLocation')}</p>
         </div>
       </div>
 
