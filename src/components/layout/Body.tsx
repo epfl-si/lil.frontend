4e33fbab1a69609ab80f0a6535815089112af688
Rosa Maggi <rosa.maggi@epfl.ch>
2026-05-12 18:09:18 +0200

    [feature] Add button to navigate towards new barcode page form

--- src/components/layout/Body.tsx
+++ src/components/layout/Body.tsx
@@ -1,9 +1,14 @@
 import {StorageTable} from "@/components/pages/StorageTable.tsx";
 import type {State} from "@epfl-si/react-appauth";
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
+import {Button} from "@/components/ui/button.tsx";
+import {ListPlus} from "lucide-react";
+import {useNavigate} from "react-router";
 
 export const Body = ({ oidc }: { oidc: State }) => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
     <div className="p-4 sm:p-8 max-w-6xl mx-auto w-full">
       <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-6 gap-4">
@@ -11,6 +16,15 @@ export const Body = ({ oidc }: { oidc: State }) => {
           <h1 className="text-2xl sm:text-3xl font-bold">{t('app.storageFacilityManagement')}</h1>
           <p className="text-gray-500 text-sm mt-1">{t('app.manageYourLocation')}</p>
         </div>
+        <Button
+          variant="outline"
+          size="lg"
+          className="primary-buttons"
+          onClick={() => navigate("/code/new")}
+        >
+          <ListPlus />
+          {t('app.addNewLocation')}
+        </Button>
       </div>
 
       <StorageTable oidc={oidc} />
