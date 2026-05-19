b3c308818a106c0cf87de0cb48a1462334241337
Rosa Maggi <rosa.maggi@epfl.ch>
2026-05-19 17:42:55 +0200

    [feature] Create Storage

--- src/components/form/Details.tsx
+++ src/components/form/Details.tsx
@@ -3,8 +3,15 @@ import type {StorageType} from "@/lib/types.tsx";
 import {useEffect, useState} from "react";
 import {Filters} from "@/components/parts/filters.tsx";
 import type {ActiveFilters} from "@/components/pages/StorageTable.tsx";
+import {Button} from "@/components/ui/button.tsx";
+import {Save} from "lucide-react";
+import {useTranslation} from "react-i18next";
+import {saveStorage} from "@/lib/graphql/postingTools.ts";
+import {useNavigate} from "react-router";
 
 export const Details = ({ oidc, details }: { oidc: State, details: StorageType | undefined }) => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
   const [activeFilters, setActiveFilters] = useState<ActiveFilters>({
     roomType: details?.roomType.symbol,
     productType: details?.productType.symbol,
@@ -19,9 +26,36 @@ export const Details = ({ oidc, details }: { oidc: State, details: StorageType |
     setActiveFilters((prev) => ({ ...prev, [key]: value}));
   }
 
+  const onSaveStorage = async () => {
+    const response = await saveStorage(
+      import.meta.env.LIL_REACT_APP_GRAPHQL_ENDPOINT_URL,
+      oidc.accessToken,
+      {
+        roomDisplay: 'INN.033',
+        roomId: 1234,
+        roomType: activeFilters.roomType,
+        productType: activeFilters.productType,
+        storageType: activeFilters.storageType,
+        storageSubType: activeFilters.storageSubType,
+      }
+    );
+    if (response.status === 200 && response.barcode) {
+      navigate(`/code/${response.barcode}`);
+    }
+  }
+
   return (
     <div style={{ display: "flex", justifyContent: "space-between", flexDirection: "row"}}>
       { activeFilters && <Filters oidc={oidc} activeFilters={activeFilters} onFilterChange={handleFilterChange} isCascading={true} />}
+      {!details &&
+          <Button
+            variant="outline"
+            size="lg"
+            onClick={onSaveStorage}
+          >
+            <Save />
+            {t('app.saveStorage')}
+          </Button>}
     </div>
   );
 };
