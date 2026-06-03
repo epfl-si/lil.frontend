66fe18c93a6f637092675d03b7c676f293d303cc
Olivier Bieler <olivier.bieler@epfl.ch>
2026-06-03 09:01:45 +0200

    [feature] Improve room search layout

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -70,8 +70,8 @@ function CommandInput({
   ...props
 }: React.ComponentProps<typeof CommandPrimitive.Input>) {
   return (
-    <div data-slot="command-input-wrapper" className="p-1 pb-0">
-      <InputGroup className="h-8! rounded-lg! border-input/30 bg-input/30 shadow-none! *:data-[slot=input-group-addon]:pl-2!">
+    <div data-slot="command-input-wrapper">
+      <InputGroup className="rounded-lg! border-input/30 bg-input/30 shadow-none! *:data-[slot=input-group-addon]:pl-2!">
         <CommandPrimitive.Input
           data-slot="command-input"
           className={cn(
