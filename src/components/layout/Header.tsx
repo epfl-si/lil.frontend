2790d1fee89d3df4d726ca612bd715d579b189d1
Jch4ipas <jeremy.wuersch@gmail.com>
2026-06-25 15:12:31 +0200

    [fix] test onLogin in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -71,7 +71,10 @@ export const Header: React.FC<HeaderProps> = ({ user, onLogin, onLogout }) => {
             ) : (
               <button
                 type="button"
-                onClick={onLogin}
+                onClick={() => {
+                  console.log("Test", import.meta.env.LIL_REACT_APP_AUTH_SERVER_URL)
+                  if (onLogin) onLogin();
+                }}
                 className="text-muted-foreground hover:text-foreground hover:cursor-pointer font-medium px-4 py-2"
               >
                 {t('header.signIn')}
