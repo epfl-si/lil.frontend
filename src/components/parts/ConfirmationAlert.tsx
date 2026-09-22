d7a14b9f43116a6bc4952b7c3eba7ece294b6b9b
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-22 14:22:14 +0200

    [feature] Add confirmation dialog for storage deletion

--- src/components/parts/ConfirmationAlert.tsx
+++ src/components/parts/ConfirmationAlert.tsx
@@ -10,22 +10,34 @@ import {
 } from "../ui/alert-dialog";
 import {useTranslation} from "react-i18next";
 import {Trash2} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
 
-export const ConfirmationAlert = ({ title, description, actionLabel, tooltip, onSubmit}: {
+export const ConfirmationAlert = ({ title, description, actionLabel, tooltip, onSubmit, isPrimaryButton, disable}: {
   title: string,
   description: string,
   actionLabel: string
   tooltip: string,
-  onSubmit: () => void
+  onSubmit: () => void,
+  isPrimaryButton?: boolean,
+  disable?: boolean
 }) => {
   const { t } = useTranslation();
 
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <span title={tooltip} className="cursor-pointer">
-          <Trash2 style={{color: "black"}}/>
-        </span>
+        {isPrimaryButton ?
+          <Button className="primary-buttons"
+                  variant="outline"
+                  disabled={disable}
+                  size="lg">
+            <Trash2 />
+            {tooltip}
+          </Button> :
+          <span title={tooltip} className="cursor-pointer">
+            <Trash2 style={{color: "black"}}/>
+          </span>
+        }
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
